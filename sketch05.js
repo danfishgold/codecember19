867e668ebc615662efbc88ba8c2953db818a805b
Dan Fishgold <d.fishgold@gmail.com>
2020-08-03 23:52:10 +0300

    Finish #5

--- sketch05.js
+++ sketch05.js
@@ -5,25 +5,29 @@ const s05 = (canvasSide) => (s) => {
   }
 
   const f = canvasSide / 800
-  const l = 30
+  const l = canvasSide / 20
   const n = l * (s.sqrt(3) / 2)
 
-  const offsetX = 0
-  const offsetY = 0
-
-  const palette = shufflePalette(
-    // 'https://coolors.co/c6ebbe-a9dbb8-7ca5b8-38369a-020887',
-    'https://coolors.co/8cb369-f4e285-f4a259-5b8e7d-bc4b51',
-  )
-  console.log(palette)
-  const colors = [palette[0], palette[1], palette[2]]
+  // https://coolors.co/757780-dedaba-387780-dbd4d3-e83151
+  const colors = ['#dedaba', '#387780', '#e83151']
 
   function color(index) {
-    return colors[(3 + index) % 3]
+    const mod = index % 3
+    if (mod < 0) {
+      return colors[3 + mod]
+    } else {
+      return colors[mod]
+    }
   }
   s.draw = () => {
     s.strokeWeight(f)
-    for (const [x, y, i, j] of hexagonalGrid(400, 400, 3 * l, 4, 0)) {
+    for (const [x, y, i, j] of hexagonalGrid(
+      canvasSide / 2,
+      canvasSide / 2,
+      3 * l,
+      Math.ceil(canvasSide / (4 * l)),
+      0,
+    )) {
       megaHexagon(x, y, (i + j) % 3)
     }
 
@@ -32,8 +36,6 @@ const s05 = (canvasSide) => (s) => {
   }
 
   function megaHexagon(x, y, colorIndex) {
-    const v1 = s.createVector(1, 0).rotate(30)
-    const v2 = s.createVector(1, 0).rotate(150)
     s.fill(color(colorIndex))
     hexagonPiece(x, y, 30)
     s.fill(color(colorIndex + 1))
@@ -45,6 +47,7 @@ const s05 = (canvasSide) => (s) => {
       if (i == 0 && j == 0) {
         continue
       }
+      s.fill(color(colorIndex + idx + 1))
       hexagonPiece(x1, y1, 60 * (-idx - 0.5))
     }
   }
@@ -57,6 +60,8 @@ const s05 = (canvasSide) => (s) => {
     s.beginShape()
     s.vertex(x, y)
     s.vertex(x + n * v1.x, y + n * v1.y)
+    s.vertex(x + n * v1.x - (l * v1.y) / 2, y + n * v1.y + (l * v1.x) / 2)
+    s.vertex(x + n * v2.x + (l * v2.y) / 2, y + n * v2.y - (l * v2.x) / 2)
     s.vertex(x + n * v2.x, y + n * v2.y)
     s.endShape(s.CLOSE)
   }
