4a5671bcbf73cef282ee17007073d943a82ccfb4
Dan Fishgold <d.fishgold@gmail.com>
2020-08-03 23:03:33 +0300

    A really fancy helper function for hexagonal grids (or lattices)

--- utils.js
+++ utils.js
@@ -19,8 +19,16 @@ function shuffle(array) {
   return array
 }
 
-function range(n) {
-  return [...Array(n).keys()]
+function range(n, m) {
+  if (m === undefined) {
+    m = n
+    n = 0
+  }
+  if (m > n) {
+    return [...Array(m - n).keys()].map((i) => n + i)
+  } else {
+    return [...Array(n - m).keys()].map((i) => n - i)
+  }
 }
 
 function drawBorder(sketch, strokeColor) {
@@ -52,4 +60,45 @@ function shufflePalette(coolorsUrl) {
     .split('-')
     .map((c) => '#' + c)
   return shuffle(colors)
+}
+
+function* hexagonalGrid(x, y, l, r, angle) {
+  angle = angle * (Math.PI / 180)
+  const x1 = l * Math.cos(angle)
+  const y1 = l * Math.sin(angle)
+  const x2 = l * Math.cos(angle + Math.PI * (2 / 3))
+  const y2 = l * Math.sin(angle + Math.PI * (2 / 3))
+  for (const [i, j, n, rr] of hexagonalGridIndexes(r)) {
+    yield [x + i * x1 + j * x2, y + i * y1 + j * y2, i, j, n, rr]
+  }
+}
+function* hexagonalGridIndexes(r) {
+  for (const rr of range(r + 1)) {
+    for (const [i, j, n] of hexagonalGridRingIndexes(rr)) {
+      yield [i, j, n, rr]
+    }
+  }
+}
+function* hexagonalGridRingIndexes(r) {
+  if (r === 0) {
+    yield [0, 0, 0]
+  }
+  for (const i of range(r)) {
+    yield [i, r, i]
+  }
+  for (const i of range(r)) {
+    yield [r, r - i, r + i]
+  }
+  for (const i of range(r)) {
+    yield [r - i, -i, 2 * r + i]
+  }
+  for (const i of range(r)) {
+    yield [-i, -r, 3 * r + i]
+  }
+  for (const i of range(r)) {
+    yield [-r, -r + i, 4 * r + i]
+  }
+  for (const i of range(r)) {
+    yield [-r + i, i, 5 * r + i]
+  }
 }
